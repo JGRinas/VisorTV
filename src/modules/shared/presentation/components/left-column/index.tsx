812b9172f1f90658195a0aaa6b4622380dbf4833
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2024-10-02 17:47:31 -0300

    feat: added left-column component

--- src/modules/shared/presentation/components/left-column/index.tsx
+++ src/modules/shared/presentation/components/left-column/index.tsx
@@ -1,8 +1,30 @@
-import { Card } from "../cards";
+import { Card, CardTemperature } from "../cards";
+import { Text, Title } from "../texts";
 import "./styles.css";
 
 export const LeftColumn = () => {
-  return <div className="left-column">
-    <Card />
-  </div>;
+  return (
+    <div className="left-column">
+      <Card icon="location">
+        <Title>Corrientes, Corrientes</Title>
+      </Card>
+      <CardTemperature />
+      <Card icon="humidity">
+        <Title>Humedad:</Title>
+        <Text>38%</Text>
+      </Card>
+      <Card icon="pressure">
+        <Title>Presión: :</Title>
+        <Text>1010.2 hPa</Text>
+      </Card>
+      <Card icon="wind">
+        <Title>Viento:</Title>
+        <Text>Sur a 14km/h</Text>
+      </Card>
+      <Card icon="visibility">
+        <Title>Visibilidad:</Title>
+        <Text>10km</Text>
+      </Card>
+    </div>
+  );
 };

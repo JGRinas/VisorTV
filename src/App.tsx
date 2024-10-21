12ed60b68cc0c260ad73fe006e1f00f023c82833
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2024-10-21 07:45:34 -0300

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PublicRoute from "./routes/public-routes";
+import { Login } from "./modules/auth/presentation/views";
+import PrivateRoute from "./routes/private-routes";
 import { Home } from "./modules/home/presentation/views";
 import "./styles/global.css";
 
-function App() {
-  return <Home />;
-}
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        {/* Ruta pública general */}
+        <Route path="/" element={<Home />} />
+        {/* Rutas públicas */}
+        <Route path="/access-to-dashboard" element={<PublicRoute />}>
+          <Route path="login" element={<Login />} />
+        </Route>
+
+        {/* Rutas privadas */}
+        <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route path="dashboard" element={<></>} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/routes/private-routes.tsx
@@ -0,0 +1,10 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useAppSelector } from "../store";
+
+const PrivateRoute = () => {
+  const { token } = useAppSelector((state) => state.auth);
+
+  return token ? <Outlet /> : <Navigate to="/" />;
+};
+
+export default PrivateRoute;

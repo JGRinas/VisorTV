8eeaf5a7777586dd7add77810e3d712b53c7d275
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2024-10-21 08:02:29 -0300

    added userMenu in header

---
+++ src/modules/shared/presentation/components/dropdown/index.tsx
@@ -0,0 +1,51 @@
+import { IUserProfileDTO } from "~/modules/auth/domain/dtos/request";
+import { FC } from "react";
+import "./styles.css"
+
+export const Dropdown = ({
+  profile,
+  dropdownVisible,
+  handleNavigation,
+}: {
+  profile: IUserProfileDTO;
+  dropdownVisible: boolean;
+  handleNavigation: (path: string) => void;
+}) => {
+  if (!profile) return null;
+
+  return (
+    <div className={`dropdown ${dropdownVisible ? "visible" : ""}`}>
+      {profile.role === "admin" && (
+        <>
+          <DropdownItem
+            label="Ver Usuarios"
+            onClick={() => handleNavigation("/users")}
+          />
+          <DropdownItem
+            label="Ver Pantallas"
+            onClick={() => handleNavigation("/screens")}
+          />
+        </>
+      )}
+      {profile.role === "operator" && (
+        <DropdownItem
+          label="Ver Pantallas"
+          onClick={() => handleNavigation("/screens")}
+        />
+      )}
+    </div>
+  );
+};
+
+interface DropdownItemProps {
+  label: string;
+  onClick: () => void;
+}
+
+export const DropdownItem: FC<DropdownItemProps> = ({ label, onClick }) => {
+  return (
+    <button className="dropdown-item" onClick={onClick}>
+      {label}
+    </button>
+  );
+};
--- src/modules/shared/presentation/components/header/index.tsx
+++ src/modules/shared/presentation/components/header/index.tsx
@@ -1,10 +1,40 @@
 import { UnneIcon } from "~/assets/icons";
 import "./styles.css";
+import { useAppSelector } from "~/store";
+import { Dropdown } from "../dropdown";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
+  const profile = useAppSelector((state) => state.auth.profile);
+  const [dropdownVisible, setDropdownVisible] = useState(false);
+  const navigate = useNavigate();
+
+  const toggleDropdown = () => {
+    setDropdownVisible((prevState) => !prevState);
+  };
+
+  const handleNavigation = (path: string) => {
+    setDropdownVisible(false);
+    navigate(path);
+  };
   return (
     <header className="headerContainer">
       <UnneIcon />
+      {profile ? (
+        <div className="userMenu">
+          <button className="dropdown-toggle" onClick={toggleDropdown}>
+            {profile.name} ({profile.role})
+          </button>
+          {
+            <Dropdown
+              dropdownVisible={dropdownVisible}
+              handleNavigation={handleNavigation}
+              profile={profile}
+            />
+          }
+        </div>
+      ) : null}
     </header>
   );
 };

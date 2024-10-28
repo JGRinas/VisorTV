ad9b42f7a60a2a10f1ef84e8c4f63e26485f4d1f
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2024-10-28 00:03:35 -0300

    added belgrano camera

---
+++ src/modules/shared/presentation/components/camera/camera.tsx
@@ -0,0 +1,14 @@
+export const CameraBridge = () => {
+  return (
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube.com/embed/nTjJbUG5K7k?si=wUsnjQC5UDFjgO48"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerpolicy="strict-origin-when-cross-origin"
+      allowfullscreen
+    />
+  );
+};

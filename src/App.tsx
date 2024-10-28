beee393dd1ba1c81792d246c18eb35aad0ae36ba
Juan Gabriel Benitez Rinas <juangabrielrinas@gmail.com>
2024-10-28 18:09:41 -0300

    added openWeatherServices

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PublicRoute from "./routes/public-routes";
 import { Login } from "./modules/auth/presentation/views";
 import PrivateRoute from "./routes/private-routes";
-import { Home } from "./modules/home/presentation/views";
+import Home from "./modules/home/presentation/views";
 import "./styles/global.css";
 import { UserList } from "./modules/admin/user_management/presentation/views/user-list";
 import ScreenBuilder from "./modules/admin/screen_management/presentation/screens/screen-builder";
+import ScreenList from "./modules/admin/screen_management/presentation/screens/screen-list";
+import ScreenEdit from "./modules/admin/screen_management/presentation/screens/screen-edit";
 
 const App = () => {
   return (
@@ -13,6 +15,7 @@ const App = () => {
       <Routes>
         {/* Ruta pública general */}
         <Route path="/" element={<Home />} />
+        <Route path="/home/:screenId?" element={<Home />} />
         {/* Rutas públicas */}
         <Route path="/access-to-dashboard" element={<PublicRoute />}>
           <Route path="login" element={<Login />} />
@@ -22,6 +25,8 @@ const App = () => {
         <Route path="/dashboard" element={<PrivateRoute />}>
           <Route path="screen-builder" element={<ScreenBuilder />} />
           <Route path="user-list" element={<UserList />} />
+          <Route path="screens" element={<ScreenList />} />
+          <Route path="screen-edit/:screenId" element={<ScreenEdit />} />
         </Route>
       </Routes>
     </Router>
